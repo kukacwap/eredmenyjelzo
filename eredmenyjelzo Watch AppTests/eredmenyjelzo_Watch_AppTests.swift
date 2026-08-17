4f95a6f90c0ee5d2ba3cc7e4cb11d630d2bb6531
kukacwap <gabor.madak@gmail.com>
2026-08-17 12:05:32 +0200

    Xcode által generált projekt a legfrissebb kóddal

---
+++ eredmenyjelzo Watch AppTests/eredmenyjelzo_Watch_AppTests.swift
@@ -0,0 +1,19 @@
+//
+//  eredmenyjelzo_Watch_AppTests.swift
+//  eredmenyjelzo Watch AppTests
+//
+//  Created by Madak Gabor on 2026. 07. 31..
+//
+
+import Testing
+@testable import eredmenyjelzo_Watch_App
+
+struct eredmenyjelzo_Watch_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
